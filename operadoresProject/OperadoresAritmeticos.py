189bc4f1b7030462683ea143060cca4b5ae7e259
Luis Vejarano <luis.vejarano@docente.fup.edu.co>
2024-09-26 19:47:03 -0500

    Se crea el proyecto con el archivo OperadoresAritmeticos

---
+++ operadoresProject/OperadoresAritmeticos.py
@@ -0,0 +1,8 @@
+# Ejemplo operadores Aritmeticos
+#numero1:int = 18
+#numero2:int = 12
+numero1 = int(input("Ingrese el numero1: "))
+numero2 = int(input("Ingrese el numero2: "))
+
+suma = numero1 + numero2
+print(f"La suma de {numero1} y {numero2} es: {suma}")
